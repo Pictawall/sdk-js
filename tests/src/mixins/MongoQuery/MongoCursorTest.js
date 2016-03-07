dcbb91803fd33aee2edceafaaaef384d6a3a90e5
Guylian Cox <guylian.cox@pictawall.com>
2016-03-07 16:51:25 +0100

    Added MongoDB tests

--- tests/src/mixins/MongoQuery/MongoCursorTest.js
+++ tests/src/mixins/MongoQuery/MongoCursorTest.js
@@ -9,7 +9,7 @@ describe('MongoCursor', () => {
     { id: 4, name: 'Zzz' },
     { id: 5, name: 'John' },
     { id: 6, name: 'Fred' },
-    { id: 12, name: 'AAA'}
+    { id: 12, name: 'AAA' }
   ];
 
   it('accepts MongoDB-like find queries', () => {
@@ -34,5 +34,39 @@ describe('MongoCursor', () => {
     expect(result.length).toBe(DATA.length);
 
     expect(result[0].name).toBe('Zzz');
+  });
+
+  it('accepts MongoDB-like limit queries', () => {
+    const cursor = new MongoCursor({}, DATA);
+
+    const result = cursor.limit(1).toArray();
+
+    expect(result).toEqual(jasmine.any(Array));
+    expect(result.length).toBe(1);
+
+    expect(result[0].id).toBe(DATA[0].id);
+  });
+
+  it('accepts MongoDB-like skip queries', () => {
+    const cursor = new MongoCursor({}, DATA);
+
+    const result = cursor.skip(1).toArray();
+
+    expect(result).toEqual(jasmine.any(Array));
+    expect(result.length).toBe(DATA.length - 1);
+
+    expect(result[0].id).toBe(DATA[1].id);
+  });
+
+  it('accepts all of the above at the same time', () => {
+    const cursor = new MongoCursor({}, DATA);
+
+    const result = cursor.skip(2).limit(2).sort({ id: -1 }).toArray();
+
+    expect(result).toEqual(jasmine.any(Array));
+    expect(result.length).toBe(2);
+
+    expect(result[0].id).toBe(5);
+    expect(result[1].id).toBe(4);
   });
 });
