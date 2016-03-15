bb719942cb5c115faa35208cf662362690337fcc
Guylian Cox <guylian.cox@pictawall.com>
2016-03-15 12:17:14 +0100

    Renamed folder tests -> test

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'tests/MainSpec.js'
+      'test/MainSpec.js'
     ],
 
     // list of files to exclude
@@ -23,7 +23,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/MainSpec.js': ['webpack', 'sourcemap']
+      'test/MainSpec.js': ['webpack', 'sourcemap']
     },
 
     webpack: (function () {
